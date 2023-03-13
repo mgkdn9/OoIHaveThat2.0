8aee3930948124579bff4484ea02f3137c1e2550
mgkdn9 <mkohlberg95@gmail.com>
2023-03-12 20:22:30 -0400

    Add project files.

---
+++ OoIHaveThat2.0/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OoIHaveThat2._0.Models;
+
+namespace OoIHaveThat2._0.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<OoIHaveThat2._0.Models.Response> Response { get; set; } = default!;
+    }
+}
---
+++ OoIHaveThat2.0/Models/Response.cs
@@ -0,0 +1,25 @@
+﻿namespace OoIHaveThat2._0.Models
+{
+    public class Response
+    {
+
+        public int Id { get; set; }
+
+        public int SeekerUserId { get; set; }
+
+        public int OwnerUserId { get; set; }
+
+        public int ToolRequestId { get; set; }
+
+        public double PriceCounterOffer { get; set; }
+
+        public double PriceCounterOfferCounter { get; set; }
+
+        public double DistanceBetween { get; set; }
+
+        public Response()
+        {
+            
+        }
+    }
+}
